1ba0e6e4ce513fc74ab3e0270d77888d682797a1
Tiago Alves <tcristianoalves@outlook.com>
2015-09-08 06:08:26 -0300

    Criação da entidade Banco

---
+++ module/cff/src/cff/Entity/Banco/Banco.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace cff\Entity\Banco;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/** @ORM\Entity */
+class Banco
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $nome;
+
+    /** @ORM\Column(type="string") */
+    protected $agencia;
+
+    /** @ORM\Column(type="integer") */
+    protected $status;
+
+
+    /** @ORM\ManyToOne(targetEntity="cff\Entity\Familia\Familia") */
+    protected $familia;
+
+    public function __construct()
+    {
+        $this->projects = new ArrayCollection();
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAgencia()
+    {
+        return $this->agencia;
+    }
+
+    /**
+     * @param mixed $agencia
+     */
+    public function setAgencia($agencia)
+    {
+        $this->agencia = $agencia;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFamilia()
+    {
+        return $this->familia;
+    }
+
+    /**
+     * @param mixed $familia
+     */
+    public function setFamilia($familia)
+    {
+        $this->familia = $familia;
+    }
+
+
+
+}
