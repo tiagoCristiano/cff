2a48dd0e44cf2b3f849ed42e40b2ef0a6bd17f5c
Tiago Alves <tcristianoalves@outlook.com>
2015-09-08 06:10:38 -0300

    Correções

--- module/cff/src/cff/V1/Rest/Bancos/BancosCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace cff\V1\Rest\Bancos;
-
-use Zend\Paginator\Paginator;
-
-class BancosCollection extends Paginator
-{
-}
--- module/cff/src/cff/V1/Rest/Bancos/BancosEntity.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace cff\V1\Rest\Bancos;
-
-class BancosEntity
-{
-}
