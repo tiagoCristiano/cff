877ba95c361e8bf08e2cee98c4bedc3d083cb3e5
Tiago Cristiano alves <tcristinoalves@outlook.com>
2015-11-10 23:35:33 -0200

    Tratamento retorno receitas

--- module/cff/src/cff/V1/Rest/Receitas/ReceitasService.php
+++ module/cff/src/cff/V1/Rest/Receitas/ReceitasService.php
@@ -18,10 +18,37 @@ class ReceitasService
         $entity = $this->em->getRepository($this->repository)
             ->findBy(array('status' => 1,'familia' => $familiaId ));
         if(!empty($entity)){
-            return $entity;
             return $this->padronizaRetorno($entity);
         }
         return false;
+    }
+
+    protected function padronizaRetorno($entity)
+    {
+        $total = 0;
+        $data = array();
+        foreach($entity as $receita) {
+            $dataLancamento = new \DateTime($receita->getDataCriacao());
+            $total += $receita->getValor();
+            $data[] = array(
+
+                'id' => $receita->getId(),
+                'descricao' =>$receita->getDescricao(),
+                'status' => $receita->getStatus(),
+                'valor' => $receita->getValor(),
+                'conta' => $receita->getConta()->getNumero(),
+                'familia' => array(
+                    'id' =>$receita->getFamilia()->getId(),
+                    'nome' =>$receita->getFamilia()->getNome()
+                ),
+                'dataLancamento' => $dataLancamento->format('d-m-Y'),
+                'usuario' => $receita->getUser()->getNome(),
+                'totalReceita' => $total,
+
+            );
+        }
+        return $data;
+
     }
 
 }
